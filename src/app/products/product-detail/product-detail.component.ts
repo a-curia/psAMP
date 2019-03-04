091e413fbacf1c4abeeeb4260991a8e4474fa4a5
Gheorghe-Adrian Curia <gheorgheadrian.curia@ihsmarkit.com>
2019-03-04 11:45:23 +0200

    Activating a Route with Code

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../product';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   // selector: 'app-product-detail', // this component will not be nested
@@ -8,12 +9,34 @@ import { IProduct } from '../product';
 })
 export class ProductDetailComponent implements OnInit {
 
-  pageTitle: string = 'Product Detail';
+  pageTitle = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { // get the parameter from the URL
+    console.log(this.route.snapshot.paramMap.get('id')); // get the value through snapshot or through observable
+    // use snapshot if you need to get the initial value of the parameter
+    // use observable if the parameter changes without leaving the page - eg. next button on the details page
+  }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id'); // use + because the parameter is provided as a string
+
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      'productId': 5,
+      'productName': 'Hammer',
+      'productCode': 'TBX-0048',
+      'releaseDate': 'May 21, 2016',
+      'description': 'Curved claw steel hammer',
+      'price': 8.9,
+      'starRating': 4.8,
+      'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png'
+    };
+  }
+
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
